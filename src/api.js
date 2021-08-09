140d5db501f743a20a7841c17aff102776b13249
Isaac Mundo <imundo90@gmail.com>
2021-08-08 21:27:36 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "https://ripley-test-isaac.herokuapp.com"; // site that doesn’t send Access-Control-*
+const url = "https://proyecto-test-isaac.herokuapp.com"; // site that doesn’t send Access-Control-*
 fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
 const urlFinal = proxyurl + url;
 export default urlFinal;
