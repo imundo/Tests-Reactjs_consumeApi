84ccf87c0968799c0e853183f7eb2898a0af22cc
Isaac Mundo <imundo90@gmail.com>
2021-08-08 21:22:36 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,8 @@ import './Header.css';
 export default function Header() {
   return (
     <div id="Header__container">
-<<<<<<< HEAD
-      <h2 id="Header__text"> APP TEST DE PRODUCTOS </h2>
-=======
-      <h2 id="Header__text"> APP TEST DE PRODUCTOS RIPLEY </h2>
->>>>>>> baa188d8b48309b9c480f407fa69200d3923b639
+
+      <h2 id="Header__text"> APP TEST DE PRODUCTOS CARRITO COMPRA </h2>
     </div>
   )
 }
